cb3b19fecee8e0ad5410175a55536733677eca40
Reşat ARIKAN <60408130+resatarikan@users.noreply.github.com>
2023-03-19 17:02:23 +0000

    add 2023-03-19 ders files

---
+++ 2023-03-19/app.js
@@ -0,0 +1,29 @@
+// pTaglari = document.getElementsByTagName('p')
+
+// const baslik = document.getElementById('baslik')
+
+// text1ler = document.getElementsByClassName("text1")
+
+// // console.log(text1ler)
+
+// text2_birinci_eleman = document.querySelector('.text2')
+
+// text2ler = document.querySelectorAll('.text2')
+
+// console.log(text2ler)
+
+// baslik_id_ile = document.querySelector('#baslik')
+
+// baslik2_icindeki_pler = document.querySelectorAll('#ikincibolum p')
+
+// console.log(baslik2_icindeki_pler)
+
+// rowlar = document.querySelectorAll('.row')
+// // console.log(rowlar)
+
+// about = document.querySelector('#about')
+
+// about.innerText = 'Hakkinda'
+
+// ilkParagraf = document.querySelector(".text-muted")
+
